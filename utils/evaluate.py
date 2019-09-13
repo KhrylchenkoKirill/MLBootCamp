6dde9ca4fd4aaa5584583f060e7e4f286fcf9f6f
Ambitious <32821691+KhrylchenkoKirill@users.noreply.github.com>
2019-09-13 18:08:06 +0300

    Update evaluate.py

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -34,7 +34,7 @@ def get_iou(boxes, values=True):
     boxAArea = area(boxA)
     boxBArea = area(boxB)
     
-    if (boxAArea == 0 or boxBArea == 0):
+    if boxAArea == 0 or boxBArea == 0:
         return 0
         
     xA = max(boxA[0], boxB[0])
